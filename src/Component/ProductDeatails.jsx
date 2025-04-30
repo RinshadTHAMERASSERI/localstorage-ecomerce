d300e2e20cbaabf40a2be54f82ea88c66a0c0df1
RinshadTM <rinshadthamerasseri@gmail.com>
2025-04-30 13:42:08 +0530

    final

--- src/Component/ProductDeatails.jsx
+++ src/Component/ProductDeatails.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { data, useParams } from 'react-router-dom'
+import { useParams } from 'react-router-dom'
 import'./ProductDetails.scss'
 import { Link  } from 'react-router-dom'
 
