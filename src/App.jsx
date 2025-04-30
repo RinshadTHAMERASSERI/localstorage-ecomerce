f93eeab7fd37d30a87897afdaaceb42d3ed466e1
RinshadTM <rinshadthamerasseri@gmail.com>
2025-04-30 13:59:04 +0530

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Fetch from './Component/Fetch'
-import ProductDetails from './Component/ProductDetails'
+// import ProductDeatails from './Component/ProductDeatails'
+import ProductDeatails from './Component/ProductDeatails1'
 import Cart from './Component/Cart'
 import Nav from './Component/Nav'
 
@@ -15,7 +16,7 @@ function App() {
         
         <Routes>
           <Route path='/' element={<Fetch search={search} />} />
-          <Route path='/details/:id' element={<ProductDetails/>}/>
+          <Route path='/details/:id' element={<ProductDeatails/>}/>
           <Route  path='/cart' element={<Cart />} />
         </Routes>
       </BrowserRouter>
